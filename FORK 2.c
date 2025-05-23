0bd5c1c5c773f320d656f2295dd2fafaaab04e87
MD AHSANUR RAHAMAN <133807853+ahsanur-official@users.noreply.github.com>
2025-05-24 00:12:18 +0600

    Update FORK 2.c

--- FORK 2.c
+++ FORK 2.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-#include<math.h>
-
-int main(){
-    int n,i;
-
-    printf("Enter the number: ");
-    scanf("%d",&n);
-
-    for(i=0;i<pow(2,n);i++){
-        printf("%d. Hello Sir!\n",i+1);
-    }
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    fork();
+    fork();
+    fork();
+    printf("hello\n");
     return 0;
 }
